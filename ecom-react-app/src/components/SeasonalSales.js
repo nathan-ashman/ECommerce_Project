c617a550770ef6088fc0fe3b778d85fc2b56a2d6
unknown <natetheman19@gmail.com>
2022-05-15 19:00:56 -0400

    set up github pages

--- ecom-react-app/src/components/SeasonalSales.js
+++ ecom-react-app/src/components/SeasonalSales.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function SeasonalSales() {
     return(
-        <section id="seasonal-sales">
+        <section id="seasonal-sales"> 
             <div className="sale-banner3 text-banner">
                 <h2 id="title-h2">SEASONAL SALE</h2>
                 <h3 id="info-h3">Winter Collection - 50% OFF!</h3>
